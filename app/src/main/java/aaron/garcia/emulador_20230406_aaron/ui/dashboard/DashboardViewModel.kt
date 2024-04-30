9292ab9aee67e3a6a4b084007d2766f9e0a63e00
Gabriel Alexander <20200636@ricaldone.edu.sv>
2024-04-30 15:36:05 -0600

    Guardar

---
+++ app/src/main/java/aaron/garcia/emulador_20230406_aaron/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package aaron.garcia.emulador_20230406_aaron.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/aaron/garcia/emulador_20230406_aaron/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package aaron.garcia.emulador_20230406_aaron.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
